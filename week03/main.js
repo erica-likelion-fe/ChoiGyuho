881e4c4357b456e19ab3d69ad2fa01cc9c49a123
gyuhochoime <cjg1999@naver.com>
2025-04-03 22:06:46 +0900

    feat: 3주차 과제 제출입니다!

--- week03/main.js
+++ week03/main.js
@@ -1,9 +1,36 @@
-const bravelikelion = {
-  name: "사자",
-  attribute: " brave",
-  color: "yellow",
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let chance = 10;
+
+const findNumber = () => {
+  while (chance > 0) {
+    let input = prompt(
+      `1부터 100 사이 숫자를 입력하세요. (남은 기회: ${chance})`
+    );
+    let changeNum = parseInt(input);
+
+    // 유효성 검사용 코드입니다.
+    if (isNaN(changeNum) || changeNum < 1 || changeNum > 100) {
+      alert("1부터 100 사이 숫자만 입력해주세요.");
+      continue;
+    }
+
+    // 맞았는지 틀렸는지 판별하는 코드입니다.
+    if (changeNum === randomNumber) {
+      alert("축하합니다! 맞추셨습니다!");
+      return;
+    } else if (changeNum > randomNumber) {
+      chance--;
+      alert(`DOWN! (남은 기회: ${chance})`);
+    } else {
+      chance--;
+      alert(`UP! (남은 기회: ${chance})`);
+    }
+
+    // 10번 안에 못 맞췄을 경우 메시지 나오게 하는 코드입니다.
+    if (chance === 0) {
+      alert("실패!");
+    }
+  }
 };
 
-const { color, ...rest } = bravelikelion;
-console.log(color);
-console.log(rest);
+findNumber();
