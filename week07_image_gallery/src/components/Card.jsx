9f044592ce600d91952f4d9145a3a8301f06587c
gyuhochoime <cjg1999@naver.com>
2025-05-16 19:59:29 +0900

    feat: Card UI 완성 및 mockData 추가

--- week07_image_gallery/src/components/Card.jsx
+++ week07_image_gallery/src/components/Card.jsx
@@ -1,7 +1,13 @@
-import _styles from "./Card.module.scss";
+import styles from "./Card.module.scss";
 
-const Card = () => {
-  return <div>card</div>;
+const Card = ({ img, title, desc }) => {
+  return (
+    <div className={styles.Card}>
+      <img className={styles.card_img} alt="card" src={img} />
+      <div className={styles.title}>{title}</div>
+      <div className={styles.desc}>{desc}</div>
+    </div>
+  );
 };
 
 export default Card;
--- week07_image_gallery/src/pages/Home.jsx
+++ week07_image_gallery/src/pages/Home.jsx
@@ -1,7 +1,39 @@
-import _styles from "./Home.module.scss";
+import { useState, useEffect } from "react";
+
+import Button from "../components/Button";
+import Card from "../components/Card";
+import styles from "./Home.module.scss";
 
 const Home = () => {
-  return <div>home</div>;
+  const [cardInfoList, setCardInfoList] = useState([]);
+
+  useEffect(() => {
+    fetch("/mocks/mockData.json")
+      .then((res) => res.json())
+      .then((result) => setCardInfoList(result.galleries));
+  }, []);
+
+  return (
+    <div className={styles.Home}>
+      <div className={styles.button_section}>
+        <Button text={"Create Post"} />
+      </div>
+      <div className={styles.card_desc}>Image</div>
+      <div className={styles.card_section}>
+        {cardInfoList.map((cardInfo) => {
+          return (
+            <Card
+              key={cardInfo.id}
+              id={cardInfo.id}
+              img={cardInfo.img}
+              title={cardInfo.title}
+              desc={cardInfo.desc}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
